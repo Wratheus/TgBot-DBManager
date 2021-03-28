dadcadd7f0688e070f4746d785287babf143bc7c
Aleksandr Pavlenko <Morwar211@gmail.com>
2021-03-28 18:57:01 +0300

    fix gui

--- GUI/main.py
+++ GUI/main.py
@@ -1,12 +1,12 @@
 from PyQt5 import QtWidgets
-from Package.interface import Ui_MainWindow as GUI
+import interface
 import sys
 
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui = GUI()
+    ui = interface.Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
